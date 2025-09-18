cbbaf0dc5370dc95e10f0b1ff5c0b5d842ea5da3
jacobsologub@gmail.com <jacobsologub@gmail.com>
2025-09-17 22:30:25 -0400

    Adding version.txt and now fetching commit hash from version.txt.

--- fetch.sh
+++ fetch.sh
@@ -6,8 +6,20 @@ gclient;
 
 if [ ! -d src ]; then
     mkdir src;
-	cd src;
-	fetch skia;
+    cd src;
+    fetch skia;
 fi
 
 cd $script_dir;
+
+# Read Skia version from version.txt
+if [ -f version.txt ]; then
+    skia_version=$(cat version.txt);
+    echo "Checking out Skia version: $skia_version";
+    cd src/skia;
+    git fetch origin;
+    git checkout $skia_version;
+    cd $script_dir;
+else
+    echo "Warning: version.txt not found, using latest Skia version";
+fi
