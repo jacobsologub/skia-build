9ca4d5bba4ed1224b6aef5aa8b3a4dfd25b124d5
jacobsologub@gmail.com <jacobsologub@gmail.com>
2025-10-02 22:00:50 -0400

    fetch.sh: Remove whitespace

--- fetch.sh
+++ fetch.sh
@@ -14,7 +14,7 @@ cd $script_dir;
 
 # Read Skia version from version.txt
 if [ -f version.txt ]; then
-    skia_version=$(cat version.txt);
+    skia_version=$(cat version.txt | tr -d '[:space:]');
     echo "Checking out Skia version: $skia_version";
     cd src/skia;
     git fetch origin;
